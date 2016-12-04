93dc2ffe5f7c03128339560cb3362ffca5cfebae
jeahyun <bee0005@naver.com>
2016-12-05 01:58:26 +0900

    update

--- routes/make_detail.js
+++ routes/make_detail.js
@@ -1,38 +1,29 @@
 var express = require('express');
-var router = express.Router();
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var router = express.Router();
+
+/* Get Models */
+var schedule_detail = require('../models/schedule_detail.js');
+
 var imgNumber = 0;
 
-var detailSchema = new Schema({
-    scheduleID: String,
-    title: String,
-    contents: String,
-    detailDate: Date,
-    commentID: [String],
-    pictureName: [String]
-});
-var Detail = mongoose.model('detail', detailSchema);
-
 router.get('/register', function (req, res) {
-    console.log(req.query.id);
-    console.log(req.query.date);
-    res.render('detail_register', {title: 'Express', schedule_id: req.params.schedule_id});
+    res.render('detail_register', {title: 'Express', schedule_id: req.query.id, schedule_date: req.query.date});
 });
 
 router.post('/add', function (req, res) {
 
-    var detail = new Detail();
+    var detail = new schedule_detail();
     //file 있는지 여부 검사
     var fileNumber = 0;
     for (x in req.files) {
         fileNumber++;
     }
 
-    detail.scheduleID = '일본여행 아이디';
+    detail.scheduleID = req.body.schedule_id;
     detail.title = req.body.title;
     detail.contents = req.body.contents;
-    detail.detailDate = new Date();
+    detail.detailDate = new Date(req.body.schedule_date);
     var loopNumber = 0;
     for (var i = 0; i < fileNumber; i++) {
         var img = 'img' + imgNumber;
@@ -76,7 +67,8 @@ router.post('/add', function (req, res) {
         imgNumber++;
     }
     imgNumber = 0;
-    res.send('File uploaded!');
+    res.redirect('/schedule/list_detail/' + req.body.schedule_id);
+    //res.send('File uploaded!');
 });
 
 
--- routes/schedule.js
+++ routes/schedule.js
@@ -3,24 +3,11 @@
  */
 var express = require('express');
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 var router = express.Router();
 
-var schema = new Schema({
-    'title' : String,
-    'startDate' : Date,
-    'endDate' : Date,
-    'location' : String,
-    'detailLocation' : String,
-    'description' : String,
-    'recommend' : Number,
-    'imageUrl' : String,
-    'author' : String,
-    'public' : Boolean
-});
-
-var schedule = mongoose.model('schedule',schema);
-
+/* Get Models */
+var schedule = require('../models/schedule.js');
+var schedule_detail = require('../models/schedule_detail.js');
 
 /* GET home page. */
 
@@ -60,19 +47,32 @@ router.get('/list_detail/:schedule_id', function(req, res){
             if(err) console.log("Error!!");
             else{
                 var dates = [];
+                var detail_check = [];
+                var detail_title = [];
+                var detail_content = [];
+                var detail_pic = [];
                 var date = new Date(schedule.startDate).getTime();
                 var date_gap = (schedule.endDate - schedule.startDate) / (60 * 60 * 24 * 1000) + 1;
-                for (var i = 0; i < date_gap; i++) {
-                    dates.push(new Date(date).toDateString());
-                    console.log(new Date(date).toString());
-                    console.log(new Date(date).toDateString());
-                    date += (24 * 60 * 60 * 1000);
-                }
-                console.log(schedule.startDate);
-                console.log(schedule.endDate);
-            }
 
-        res.render('schedule_list_detail', { title: 'Schedule Detail', schedule: schedule, date_gap: date_gap, dates: dates });
+                schedule_detail.find({scheduleID: req.params.schedule_id }, function (err, details) {
+                    for (var i = 0; i < date_gap; i++) {
+                        dates.push(new Date(date).toDateString());
+                        for(var j =0; j < details.length; j++){
+                            if(details[j].detailDate.toDateString() ===  new Date(date).toDateString()){
+                                detail_check[i] = true;
+                                detail_title[i] = details[j].title;
+                                detail_content[i] = details[j].contents;
+                                detail_pic[i] = details[j].pictureName;
+                                break;
+                            }
+                        }
+                        date += (24 * 60 * 60 * 1000);
+                    }
+                    console.log(detail_pic);
+                    res.render('schedule_list_detail', { title: 'Schedule Detail', schedule: schedule, date_gap: date_gap, dates: dates,
+                                                        check:detail_check, detail_title: detail_title, detail_content: detail_content, detail_pic : detail_pic});
+                });
+            }
     });
 });
 
