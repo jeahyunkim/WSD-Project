527f6f2ba18ee8a0b3967a3fe54191a987efed1f
jeahyun <bee0005@naver.com>
2016-12-05 14:32:52 +0900

    update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Port Setting
-app.set('port', process.env.PORT || 9000);
+app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
