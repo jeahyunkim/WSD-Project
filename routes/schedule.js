cdb3818aa6e6587463385d44f41392583b0cbe67
leejinho <jinho0229@gmail.com>
2016-12-05 17:09:04 +0900

    file empty modify

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,7 +4,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var assert = require('assert');
+var multer = require('multer');
+var path = require('path');
 var router = express.Router();
+
+var storage = multer.diskStorage({
+    destination: function (request, file, callback) {
+        console.log(process.cwd());
+        callback(null, path.join(process.cwd()+'/uploads/'));
+    },
+    filename: function (request, file, callback) {
+        console.log(file);
+        var fileName = Date.now() + file.originalname;
+        console.log('aa : '+fileName);
+        callback(null, fileName);
+        request.body.fileName = fileName;
+    }
+});
+
+var upload = multer({storage: storage}).single('photo');
 
 var schema = new Schema({
     'title' : String,
@@ -16,6 +35,7 @@ var schema = new Schema({
     'recommend' : Number,
     'imageUrl' : String,
     'author' : String,
+    'writeDate' : String,
     'public' : Boolean
 });
 
@@ -29,30 +49,42 @@ router.get('/register', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
-    var addSchedule = new schedule();
-    addSchedule.title = req.body.title;
-    addSchedule.location = req.body.location;
-    addSchedule.detailLocation = req.body.detailLocation;
-    addSchedule.startDate = req.body.startDate;
-    addSchedule.endDate = req.body.endDate;
-    addSchedule.description = req.body.description;
-    addSchedule.recommend = 0;
-    addSchedule.imageUrl = ' ';
-    addSchedule.author = ' ';
-    addSchedule.public = req.body.publicType;
 
-    addSchedule.save(function (err) {
-        if(err) console.log("Something went wrong while saving the thing");
-        else console.log("Thing was successfully saved");
+    upload(req,res, function (err) {
+       if(err){
+           console.log('ERROR');
+           console.log(err);
+           return;
+       }
+        console.log(req);
+        var currentDate = new Date();
+        var addSchedule = new schedule();
+        addSchedule.title = req.body.title;
+        addSchedule.location = req.body.location;
+        addSchedule.detailLocation = req.body.detailLocation;
+        addSchedule.startDate = req.body.startDate;
+        addSchedule.endDate = req.body.endDate;
+        addSchedule.description = req.body.description;
+        addSchedule.recommend = 0;
+        addSchedule.imageUrl = req.file.filename;
+        addSchedule.author = ' ';
+        addSchedule.writeDate = currentDate.getFullYear() +'-'+(currentDate.getMonth()+1)+'-'+currentDate.getDate() ;
+        addSchedule.public = req.body.publicType;
+        addSchedule.save(function (err,result) {
+            if(err) console.log("Something went wrong while saving the thing");
+            else console.log("Thing was successfully saved");
+            console.log(result);
+        });
     });
 
     res.redirect('/schedule/list');
 });
 
 router.get('/list', function(req, res, next){
-
-
-    res.render('schedule_list', { title: 'Express' });
+    schedule.find({author:' '},function (err, result) {
+        assert.equal(err,null);
+        res.render('schedule_list', { title: 'Express' ,scheduleList: result});
+    });
 });
 
 
