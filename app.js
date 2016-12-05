86a07015006e03966d2b12c22b02cba62cecc792
NamKiwook <listenme1@naver.com>
2016-12-06 01:29:57 +0900

    login session 구현

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,6 +18,15 @@ var connection = mongoose.connect('mongodb://52.78.124.66:27017/triptter');
 var make_detail = require('./routes/make_detail');
 // var fileUpload = require('express-fileupload');
 var app = express();
+
+var store = new MongoDBStore({
+  uri: "mongodb://52.78.124.66:27017/triptter",
+  collection: 'Sessions'
+});
+store.on('error', function(error){
+  assert.ifError(error),
+      assert.ok(false)
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +42,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'WSD-Project',
+  resave: false,
+  store: store,
+  saveUninitialized: false
+}));
+
 // app.use(fileUpload());
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var session = require('express-session');
 
 var User = require('../models/user.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.session.id);
+  if(req.session.userInfo != null)
+    console.log(req.session.userInfo.user_id);
   res.render('index', { title: 'Express' });
 });
 
@@ -14,6 +18,26 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
+
+
+router.post('/login',function(req, res, next){
+  User.findOne({id : req.body.user_id, password : req.body.user_pw},function(err,docs){
+    if(err) return console.log("auth err");
+    if(!docs) {
+      res.redirect('/login');
+      return console.log("user not find");
+    }else {
+      req.session.userInfo = {user_id : req.body.user_id};
+      req.session.save(function (err) {
+        res.redirect('/');
+      })
+    }
+  })
+})
+
+
+
+
 router.post('/login/register',function(req,res,next) {
   if (req.body.password == req.body.passwordConfirm) {
     var user = new User({
