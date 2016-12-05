9335cdda759903e3c1537d8b7e9a7ff271a43e25
NamKiwook <listenme1@naver.com>
2016-12-06 02:22:20 +0900

    login session 구현

--- routes/index.js
+++ routes/index.js
@@ -16,46 +16,47 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  if(req.session.userInfo != null) {
+    res.redirect('/');
+  }else {
+    res.render('login', {title: 'Express', loginFail: false, registerSuc: false, registerFail: false});
+  }
 });
 
 
 router.post('/login',function(req, res, next){
-  User.findOne({id : req.body.user_id, password : req.body.user_pw},function(err,docs){
-    if(err) return console.log("auth err");
-    if(!docs) {
-      res.redirect('/login');
-      return console.log("user not find");
-    }else {
-      req.session.userInfo = {user_id : req.body.user_id};
-      req.session.save(function (err) {
-        res.redirect('/');
-      })
-    }
-  })
-})
-
-
-
-
-router.post('/login/register',function(req,res,next) {
-  if (req.body.password == req.body.passwordConfirm) {
-    var user = new User({
-      userName: req.body.userName,
-      id: req.body.ID,
-      password: req.body.password
-    });
-    user.save(function(err,silence){
-      if(err){
-        console.log(err);
-        res.status(500).send('update error');
-        return;
+  if(req.body.but == "LOGIN") {
+    User.findOne({id: req.body.user_id, password: req.body.user_pw}, function (err, docs) {
+      if (err) return console.log("auth err");
+      if (!docs) {
+        res.render('login', {title: 'Express', loginFail: true, registerSuc: false, registerFail: false});
+        return console.log("user not find");
+      } else {
+        req.session.userInfo = {user_id: req.body.user_id};
+        req.session.save(function (err) {
+          res.redirect('/');
+        })
       }
-      res.redirect('/login');
-    });
-  }else{
-    res.redirect('/login');
+    })
+  } else if(req.body.but == "REGISTER"){
+    if (req.body.password == req.body.passwordConfirm) {
+      var user = new User({
+        userName: req.body.userName,
+        id: req.body.ID,
+        password: req.body.password
+      });
+      user.save(function(err,silence){
+        if(err){
+          console.log(err);
+          res.status(500).send('update error');
+          return;
+        }
+        res.render('login',{ title: 'Express' , loginFail : false,registerSuc : true, registerFail : false})
+      });
+    }else{
+      res.render('login',{ title: 'Express' , loginFail : false,registerSuc : false, registerFail : true})
+    }
   }
-});
+})
 
 module.exports = router;
