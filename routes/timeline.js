d3055c16ffdabe8bc711283f751980c0ee7ae8de
jeahyun <bee0005@naver.com>
2016-12-06 14:58:12 +0900

    add timeline

--- routes/timeline.js
+++ routes/timeline.js
@@ -6,10 +6,30 @@ var session = require('express-session');
 /* Get Models */
 var schedule_detail = require('../models/schedule_detail.js');
 var schedule = require('../models/schedule.js');
+var comment = require('../models/comment.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('timeline', { title: 'Express' });
+  schedule_detail.find({}).sort({detailDate: 'desc'}).exec(function(err, details){
+    comment.find({},function (err, comments) {
+      res.render('timeline', { title: 'Time Line', order: 'Date', details: details, comments: comments });
+    });
+  });
+});
+
+router.get('/recommend', function(req, res, next) {
+  schedule_detail.find({}).sort({detailDate: 'desc'}).exec(function(err, details){
+    comment.find({},function (err, comments) {
+      res.render('timeline', { title: 'Time Line', order: 'Recommend', details: details, comments: comments });
+    });
+  });
+});
+router.get('/comments', function(req, res, next) {
+  schedule_detail.find({}).sort({detailDate: 'desc'}).exec(function(err, details){
+    comment.find({},function (err, comments) {
+      res.render('timeline', { title: 'Time Line', order: 'Comments', details: details, comments: comments });
+    });
+  });
 });
 
 module.exports = router;
