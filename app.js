6b589fd43f1ee35629cf1bdf47eccf434df63710
Gyuchul Back <bgc8214@ajou.ac.kr>
2016-12-08 17:43:47 +0900

    login menu + logout

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ var make_detail = require('./routes/make_detail');
 var app = express();
 
 var store = new MongoDBStore({
-  uri: "mongodb://52.78.124.66:27017/triptter",
-  collection: 'Sessions'
+    uri: "mongodb://52.78.124.66:27017/triptter",
+    collection: 'Sessions'
 });
-store.on('error', function(error){
-  assert.ifError(error),
-      assert.ok(false)
+store.on('error', function (error) {
+    assert.ifError(error),
+        assert.ok(false)
 });
 
 // view engine setup
@@ -42,49 +42,56 @@ app.set('port', process.env.PORT || 9000);
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'WSD-Project',
-  resave: false,
-  store: store,
-  saveUninitialized: false
+    secret: 'WSD-Project',
+    resave: false,
+    store: store,
+    saveUninitialized: false
 }));
 
 // app.use(fileUpload());
+app.use(function (req, res, next) {
+    if (req.session.userInfo != null)
+        res.locals.login = true;
+    else
+        res.locals.login = false;
+    next();
+});
 
 app.use('/', index);
-app.use('/search',search);
+app.use('/search', search);
 app.use('/users', users);
 app.use('/plan', make_plan);
-app.use('/schedule',schedule);
-app.use('/schedule/detail',detail_schedule);
-app.use('/mypage',mypage);
-app.use('/timeline',timeline);
+app.use('/schedule', schedule);
+app.use('/schedule/detail', detail_schedule);
+app.use('/mypage', mypage);
+app.use('/timeline', timeline);
 app.use(fileUpload());
-app.use('/detail',make_detail);
+app.use('/detail', make_detail);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
 
 //Create Server
-var server = app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
 });
